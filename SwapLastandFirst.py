57cc8f6c8dd6239dbc8905d2ff65e48e31208bef
Shriya1995 <51079199+Shriya1995@users.noreply.github.com>
2019-09-16 20:05:23 +0530

    First commit

---
+++ SwapLastandFirst.py
@@ -0,0 +1,16 @@
+def reverseList(newList):
+  size = len(newList) 
+  temp = newList[0] 
+  newList[0] = newList[size - 1] 
+  newList[size - 1] = temp 
+  return newList 
+
+n = int(input("Enter number of elements in list"))
+li = []
+for x in range(0,n):
+  val = int(input("Enter element for list"))
+  li.append(val)
+print("List Before")
+print(li)
+print("List After")
+print(reverseList(li))
